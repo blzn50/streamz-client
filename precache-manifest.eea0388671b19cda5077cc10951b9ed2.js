1dcd0165a890d3dac5f0c148b1b1cb214387921d
naren <blazeraze.naren@gmail.com>
2018-11-04 15:33:40 +0200

    Updates

---
+++ precache-manifest.eea0388671b19cda5077cc10951b9ed2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e661edea97342f2883cf1cb9e80ccdf8",
+    "url": "/streamz-client/static/media/appStore.e661edea.png"
+  },
+  {
+    "revision": "fd2baf4bdda28174e66d",
+    "url": "/streamz-client/static/css/main.5a400553.chunk.css"
+  },
+  {
+    "revision": "213dbfd740fb1fcc34df3606bb078e15",
+    "url": "/streamz-client/static/media/videoPlaceholder.213dbfd7.png"
+  },
+  {
+    "revision": "79b26b104130e4bbb02d",
+    "url": "/streamz-client/static/js/1.79b26b10.chunk.js"
+  },
+  {
+    "revision": "c0c80a0f46effc92e952",
+    "url": "/streamz-client/static/js/runtime~main.c0c80a0f.js"
+  },
+  {
+    "revision": "9cd963f3b1e94d71f361d50b022e43aa",
+    "url": "/streamz-client/static/media/team.placeholder.9cd963f3.png"
+  },
+  {
+    "revision": "fd2baf4bdda28174e66d",
+    "url": "/streamz-client/static/js/main.fd2baf4b.chunk.js"
+  },
+  {
+    "revision": "e760ce4999506944c72c3acbee4176dd",
+    "url": "/streamz-client/static/media/playStore.e760ce49.png"
+  },
+  {
+    "revision": "f81a6f373c244b1f70f4b7402b5ab372",
+    "url": "/streamz-client/static/media/reactLogo.f81a6f37.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/streamz-client/static/media/reduxLogo.59c46753.png"
+  },
+  {
+    "revision": "e3f9f5f051fc851aef94bc91b9f0f5b1",
+    "url": "/streamz-client/static/media/nodeLogo.e3f9f5f0.png"
+  },
+  {
+    "revision": "79b26b104130e4bbb02d",
+    "url": "/streamz-client/static/css/1.00fb5860.chunk.css"
+  },
+  {
+    "revision": "070a5590c65bd43116e862226777acd3",
+    "url": "/streamz-client/index.html"
+  }
+];
